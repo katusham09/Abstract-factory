c4fe460b2a5ce95e305a8da29015c0c4379fa25d
Ekaterina <katusham0912@gmail.com>
2024-05-13 16:04:03 +0700

    Исходный предложенный код
    Работает без ошибок

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "unit.h"
+#include "classunit.h"
+#include "printoperatorunit.h"
+#include "methodunit.h"
+#include <QApplication>
+#include "iostream"
+#include "string.h"
+
+std::string generateProgram()
+{
+    ClassUnit myClass( "MyClass" );
+    myClass.add(
+        std::make_shared< MethodUnit >( "testFunc1", "void", 0 ), ClassUnit::PUBLIC);
+    myClass.add(
+        std::make_shared< MethodUnit >( "testFunc2", "void", MethodUnit::STATIC ), ClassUnit::PRIVATE);
+    myClass.add(
+        std::make_shared< MethodUnit >( "testFunc3", "void", MethodUnit::VIRTUAL | MethodUnit::CONST ), ClassUnit::PUBLIC);
+    auto method = std::make_shared< MethodUnit >( "testFunc4", "void", MethodUnit::STATIC );
+    method->add( std::make_shared< PrintOperatorUnit >( R"(Hello, world!\n)" ) );
+    myClass.add( method, ClassUnit::PROTECTED );
+    return myClass.compile();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    std::cout << generateProgram() << std::endl;
+    return 0;
+}
+
+
